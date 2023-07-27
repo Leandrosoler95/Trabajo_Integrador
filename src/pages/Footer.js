c7582b94093c9b996205f400e84b679f918105fc
Leandrosoler <leandrosoler95@gmail.com>
2023-07-27 11:28:39 -0300

    Implementacion Footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,7 +5,41 @@ import imagen from '../Imagenes//Electricidad-Electronica.jpg';
 const Footer = () => {
   return (
     <div className="footer">
-      Footer
+      <ul className="nav-list">
+          <lu>
+            <Link className="link">QUIENES SOMOS</Link>
+          </lu>
+          <lu>
+            <Link className="link">PREGUNTAS FRECUENTES</Link>
+          </lu>
+          <lu>
+            <Link className="link">CONTACTO</Link>
+          </lu>
+      </ul>    
+        
+      <ul className="nav-center">
+          <lu>
+            <Link className="link">SUCURSALES</Link>
+          </lu>
+          <lu>
+            <Link className="link">TERMINOS Y CONDICIONES</Link>
+          </lu>
+          <lu>
+            <Link className="link">FORMAS DE ENVIO</Link>
+          </lu>
+      </ul> 
+
+      <ul className="nav-right">
+          <lu>
+            <Link className="link">COMO COMPRAR</Link>
+          </lu>
+          <lu>
+            <Link className="link">TRABAJA CON NOSOTROS</Link>
+          </lu>
+          <lu>
+            <Link className="link">FORMAS DE PAGO</Link>
+          </lu>
+      </ul> 
     </div>
   )
 };
