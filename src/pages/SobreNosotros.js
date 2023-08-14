325c10380f2b7878caba2c8e06b3586245686708
Leandrosoler <leandrosoler95@gmail.com>
2023-08-14 19:59:53 -0300

    Mejora Sobre Nosotros

--- src/pages/SobreNosotros.js
+++ src/pages/SobreNosotros.js
@@ -3,21 +3,23 @@ const SobreNosotros = () => {
         <div className="sobre-nosotros">
             <h1 className="titulo-nosotros">SOBRE NOSOTROS</h1><p/>
             <h2>HISTORIA</h2>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum porttitor nibh, 
-            ac eleifend nisl. Sed lobortis interdum neque, facilisis scelerisque erat lacinia at. 
-            Phasellus ullamcorper enim ac dignissim commodo. Cras rhoncus fringilla sapien. 
-            In sed libero vitae massa vestibulum porttitor sit amet vel risus. 
-            Ut pellentesque elit et metus interdum, eget sollicitudin justo lacinia. 
-            Ut lacinia in quam ac luctus. 
-            Vestibulum ultrices orci massa, nec tempor ex lacinia vel.</h4><p/>
+            <h4>Hace 20 años  en Posadas, Argentina con una visión audaz y un espíritu emprendedor un grupo de jóvenes apasionados
+                 por la tecnología decidió fundar Misiotrónica con el objetivo de satisfacer las necesidades tecnológicas de la comunidad.
+                Desde sus humildes comienzos, la empresa se ha convertido en un referente en el sector, ofreciendo productos de alta calidad y un servicio excepcional. 
+                Su compromiso con la excelencia y la innovación ha sido la clave de su éxito.
+                La empresa comenzó como una pequeña tienda en el centro de Posadas, pero rápidamente se expandió a medida que su reputación crecía.
+                La empresa se especializa en la venta de una amplia gama de insumos tecnológicos, desde componentes de pc hasta accesorios electrónicos. 
+                A lo largo de los años, la empresa ha enfrentado desafíos y ha superado obstáculos, pero siempre ha mantenido su pasión por la tecnología y
+                su compromiso con la satisfacción del cliente. Han logrado mantenerse a la vanguardia de la industria, adaptándose a los constantes cambios tecnológicos y
+                ofreciendo soluciones innovadoras.
+                Hoy en día, esta empresa sigue creciendo y expandiéndose. Han logrado establecer alianzas estratégicas con importantes marcas tecnológicas a nivel mundial,
+                lo que les permite ofrecer productos exclusivos y precios competitivos.</h4><p/>
             <h2>MISION</h2>
-            <h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum porttitor nibh, 
-            ac eleifend nisl. Sed lobortis interdum neque, facilisis scelerisque erat lacinia at. 
-            Phasellus ullamcorper enim ac dignissim commodo. Cras rhoncus fringilla sapien. </h4><p/>
+            <h4> Nuestra misión es brindar soluciones tecnológicas innovadoras y de calidad para satisfacer las necesidades de nuestros clientes en el ámbito de los insumos
+                 tecnológicos. Nos comprometemos a ofrecer productos de última generación, acompañados de un excelente servicio al cliente y asesoramiento especializado. </h4><p/>
             <h2>VISION</h2>
-            <h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum porttitor nibh, 
-            ac eleifend nisl. Sed lobortis interdum neque, facilisis scelerisque erat lacinia at. 
-            Phasellus ullamcorper enim ac dignissim commodo. Cras rhoncus fringilla sapien. </h4>
+            <h4>Misiotrónica busca convertirse en un referente en el mercado, reconocida por su calidad, innovación y excelencia en el servicio. Aspira a ser líder en la
+                 industria tecnológica y a mantenerse a la vanguardia de los avances tecnológicos, ofreciendo siempre productos y servicios de última generación. </h4>
         </div>
     )
   };
